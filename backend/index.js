2d5d53c7fbf6418135051c3e8e85ce83caefbdc4
chxnmay17 <chxnmay17@gmail.com>
2025-07-07 18:37:19 +0530

    creating user router

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const rootRouter = require("./routes/index");
+const userRouter= require("./routes/user");
 const app = express();
 app.use("api/v1",rootRouter);
+app.use("api/v1/user",userRouter);
 
