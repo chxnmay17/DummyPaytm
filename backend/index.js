38eeca1ab235ae427d9c4a4754efdbda7b621774
chxnmay17 <chxnmay17@gmail.com>
2025-07-07 18:44:47 +0530

    adding bodyparser and cors middleware

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const rootRouter = require("./routes/index");
 const userRouter= require("./routes/user");
 const app = express();
+const cors=require("cors");
+app.use(cors());
 app.use("api/v1",rootRouter);
 app.use("api/v1/user",userRouter);
 
